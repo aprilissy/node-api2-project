a6676d849cc0fff8b642d93c74b48a449f24bc4b
April Darger <april.m.darger@gmail.com>
2020-12-01 16:53:27 -0700

    Got second post endpoint working to add comment.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -18,4 +18,22 @@ router.post('/', (req, res) => {
     })
 })
 
+router.post('/:id', async (req, res) => {
+try{ const id = Number(req.params.id)
+  const checkL = await Poster.findById(id)
+  if( checkL.length < 1) {
+    res.status(404).json({ message: "The post with the specified ID does not exist." })
+  } else if (!req.body.text){
+    res.status(400).json({ errorMessage: "Please provide text for the comment." })
+  } else {
+    const comment = {post_id:id, ...req.body}
+    const posted = await Poster.insertComment(comment)
+    res.status(201).json(posted)
+  }
+} catch(error){
+    console.trace("CREATE ERROR: ", error)
+    res.status(500).json({ errorMessage: "There was an error while saving the comment to the database." })
+  }
+})
+
 module.exports = router
